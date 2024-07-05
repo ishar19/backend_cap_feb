84205da28eadb1629409d171aa76d99ad8e0aa40
ishar19 <isharjain19@gmail.com>
2024-07-05 22:27:58 +0530

    class 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -10,17 +11,36 @@ const authRoutes = require("./routes/auth");
 dotenv.config();
 const PORT = process.env.PORT;
 
+// log every incoming request
+// store it in a file
+app.use((req, res, next) => {
+    const log = `${req.method} - ${req.url} - ${req.ip} - ${new Date()}/n`;
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    next();
+});
 
 app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
+// error handling middleware
+app.use((err, req, res, next) => {
+    let log;
+    log = err.stack;
+    log += `/n${req.method} - ${req.url} - ${req.ip} - ${new Date()}/n`;
+    fs.appendFile("error.txt", log, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.status(500).send("Something went wrong");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    mongoose.connect('mongourl').then(() => "MongoDB connected").catch(err => console.log(err));
+    mongoose.connect('mongodb+srv://user_001:iQkx70CQfF3hm8PG@cluster0.iefo3jp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => "MongoDB connected").catch(err => console.log(err));
 });
 
-// password security
-// login
-// middleware and logging 
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,20 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../schemas/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+router.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            res.status(400).json({ message: "Wrong email or password" });
+        }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Wrong email or password" });
+        }
+        else {
+            const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+            res.header("auth-token", token).json({ message: "Logged in successfully" });
+        }
+    }
+    catch (err) {
+        next(err);
+    }
 
+})
 
 router.post("/register", async (req, res) => {
     const { name, email, password, mobile } = req.body;
-    console.log(req.body);
     const user = await User.findOne({ email });  // it returns a promise
-    console.log(user);
     if (user) {
         return res.status(400).json({ message: "User already exists" });
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
         mobile
     });
     await newUser.save();
